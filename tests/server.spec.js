043b6d54ad4850f4b2d4f086f0f08bb7db0b2513
chrispetito <christopherpetito@gmail.com>
2019-07-11 15:26:25 -0700

    add tests for GET

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -5,4 +5,13 @@ describe('Server', () => {
     it('should be running on Production env', () => {
         expect(process.env.DB_ENV).toBe('production')
     })
+    describe('GET /', () => {
+        it('should respond with a 200 status code', () => {
+            return supertest(server).get('/').expect(200);
+        })
+        it('should respond with html', () => {
+            return supertest(server).get('/').expect('content-type', /html/i)
+        })
+    })
+
 })
