39fdaeb486a83c55656430b2a2f4beb17c13385e
chrispetito <christopherpetito@gmail.com>
2019-07-11 14:05:04 -0700

    deploy backend; add projects seed

---
+++ data/seeds/001-projects.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('projects').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('projects').insert([
+        {name: 'Lambda App Store 1', github: 'https://github.com/LambdaSchool', short_description: 'Lambda App Store labs project 1'},
+        {name: 'Lambda App Store 2', github: 'https://github.com/LambdaSchool2', short_description: 'Lambda App Store labs project 2'},
+        {name: 'Lambda App Store 3', github: 'https://github.com/LambdaSchool3', short_description: 'Lambda App Store labs project 3'},
+      ]);
+    });
+};
